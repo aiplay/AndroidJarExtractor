2108422db3e7ff9868fdc5c1b2966d5ffbf0dcef
aiplay <aiplayx@gmail.com>
2015-10-09 19:42:57 +0800

    添加获取jar名称以及创建tmp目录的代码

--- extractor.py
+++ extractor.py
@@ -5,11 +5,20 @@ import os
 import sys
 import shutil
 
+TOOL_PATH = os.getcwd()
+
 def create_tmp_dirs():
     print ("create tmp dirs in")
-    jar_path = sys.argv[1]
-    base = os.path.basename(jar_path)
-    print (base)
+    if len(sys.argv) == 2:
+        jar_path = sys.argv[1]
+    else:
+        print ('miss input path!')
+    file_name = os.path.basename(jar_path)
+    jar_name = os.path.splitext(file_name)[0]
+    tmp_path = TOOL_PATH + '/' + jar_name + '_tmp'
+    if os.path.exists(tmp_path):
+        shutil.rmtree(tmp_path)
+    os.mkdir(tmp_path)
 
 def main():
     print ('extractor main in')
